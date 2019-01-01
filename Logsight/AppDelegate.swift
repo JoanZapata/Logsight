bbc1595fe7a05ae7c878621de59b8f19c05d8fd0
Joan Zapata <zapata.joan@gmail.com>
2019-01-01 15:29:30 +0100

    Initial Commit

---
+++ Logsight/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Logsight
+//
+//  Created by Joan Zapata on 01/01/2019.
+//  Copyright © 2019 Joan Zapata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
